676c2a14dcf56e3c2a8835c49373c7c8ec9f0cd0
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-11-14 23:07:15 +0200

    Added second sequence using for loop

--- While/Program.cs
+++ While/Program.cs
@@ -5,6 +5,13 @@ n = uint.Parse(Console.ReadLine());
 
 for (int i = 1; i <= n; i++)
 {
-    a += Math.Pow(-1, i) / (2 * i + 1);
+    a += Math.Pow(-1, i) / (2.0 * i + 1);
 }
+
+for(int i = 1; i <= n; i++)
+{
+    b += 1 + (1.0 / Math.Pow(i, 2));
+}
+
 Console.WriteLine($"a = {a}");
+Console.WriteLine($"b = {b}");
