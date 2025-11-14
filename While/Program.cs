46077f8dc8465a1704add065c9afb1f4e4534663
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-11-14 23:03:27 +0200

    User input and first seaquence

--- While/Program.cs
+++ While/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿uint n;
+double a = 0, b = 0, c = 0;
+Console.Write("Enter n: ");
+n = uint.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    a += Math.Pow(-1, i) / (2 * i + 1);
+}
+Console.WriteLine($"a = {a}");
