574f36d71cd8b829cdbe29a81bbd3aa50b59e146
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-11-14 22:12:04 +0200

    Added user input for coeffitients

--- QuadraticEquation/Program.cs
+++ QuadraticEquation/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a, b, c;
+
+Console.WriteLine("Quadratic equation (ax^2 + bx + c = 0)");
+
+Console.WriteLine("Enter a: ");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter b: ");
+b = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter c: ");
+c = int.Parse(Console.ReadLine());
