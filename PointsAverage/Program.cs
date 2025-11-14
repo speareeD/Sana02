efb148d360a907ff40bab3da86ab61480a5c7e1c
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-11-14 22:46:29 +0200

    For loop to calcutale sum of all grades by user input, calculating average score and using if else to give the right output

--- PointsAverage/Program.cs
+++ PointsAverage/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿int sum = 0;
+Console.WriteLine("Enter 5 grades (0-100): ");
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write($"Enter {i} grade: ");
+    sum += int.Parse(Console.ReadLine());
+}
+int average = sum / 5;
+
+if(average >= 50)
+{
+    Console.WriteLine($"You are able to go to exam! Your average score is: {average}");
+} else
+{
+    Console.WriteLine($"You can't go to exam. Your average grade is too low: {average}");
+}
