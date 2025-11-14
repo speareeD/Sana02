23ba2717a531a7ff8b6ef8fa0aad6554bc01db36
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-11-14 22:54:51 +0200

    User input and check input with switch operator to give right output

--- SwitchValues/Program.cs
+++ SwitchValues/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿Console.Write("Enter digit to transform it into a word (0-9): ");
+int input = int.Parse(Console.ReadLine());
+
+switch (input)
+{
+    case 0:
+        Console.WriteLine("Zero");
+        break;
+    case 1:
+        Console.WriteLine("One");
+        break;
+    case 2:
+        Console.WriteLine("Two");
+        break;
+    case 3:
+        Console.WriteLine("Three");
+        break;
+    case 4:
+        Console.WriteLine("Four");
+        break;
+    case 5:
+        Console.WriteLine("Five");
+        break;
+    case 6:
+        Console.WriteLine("Six");
+        break;
+    case 7:
+        Console.WriteLine("Seven");
+        break;
+    case 8:
+        Console.WriteLine("Eight");
+        break;
+    case 9:
+        Console.WriteLine("Nine");
+        break;
+    default:
+        Console.WriteLine("Wrong number!");
+        break;
+}
